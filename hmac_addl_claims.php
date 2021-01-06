c8e0178e783ba6065dd7b6358675b7f259cabe59
Dan Moore <github@mooreds.com>
2021-01-06 14:24:23 -0700

    Initial commit of these jwt examples.

---
+++ hmac_addl_claims.php
@@ -0,0 +1,39 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+use \Firebase\JWT\JWT;
+
+$key = "hello php-folk!!";
+
+// User API
+$exp = time() + (60*5);
+$payload = array(
+    "iss" => "hacked.fusionauth.io", 
+    "exp" => $exp, 
+    "aud" => "238d4793-70de-4183-9707-48ed8ecd19d9",
+    "sub" => "19016b73-3ffa-4b26-80d8-aa9287738677",
+    "name" => "Dan Moore",
+    "roles" => ["RETRIEVE_TODOS"]
+);
+
+$jwt = JWT::encode($payload, $key);
+print($jwt);
+print("\n\n");
+
+// Todo API
+$decoded = JWT::decode($jwt, $key, array('HS256'));
+
+$expected_iss = "fusionauth.io";
+$expected_aud = "238d4793-70de-4183-9707-48ed8ecd19d9";
+
+if ($expected_iss != $decoded->iss) {
+  throw new UnexpectedValueException('Issuer incorrect');
+}
+
+if ($expected_aud != $decoded->aud) {
+  throw new UnexpectedValueException('Audience incorrect');
+}
+
+print_r($decoded);
+
+?>
